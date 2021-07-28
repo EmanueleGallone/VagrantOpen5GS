ed2f8e0ebc2ee250b9be6854770ebded5bdedbb6
Em-G <26747011+EmanueleGallone@users.noreply.github.com>
2021-07-28 15:39:09 +0200

    Update post-config.sh

--- post-config.sh
+++ post-config.sh
@@ -44,6 +44,9 @@ sudo sysctl -w net.ipv6.conf.all.forwarding=1
 sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 sudo ip6tables -t nat -A POSTROUTING -s 2001:230:cafe::/48 ! -o ogstun -j MASQUERADE
 
+log "disabling firewall"
+sudo ufw disable
+
 
 log "End post-config script"
 
